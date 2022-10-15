abe5e1dc08a8a9e1f2f14857de9e21c67bd5a3ae
Wei Zhang <zhang.wei8@northeastern.edu>
2022-10-15 16:47:06 -0400

    Add monaco editor to this project.

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,16 @@ import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 
 const App = () => {
   const ref = useRef<any>();
   const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
-    ref.current = await esbuild.startService({
+    ref.current = await esbuild.initialize({
       worker: true,
       wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
@@ -68,6 +68,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
@@ -75,7 +76,6 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
       <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html}/>
     </div>
   );
