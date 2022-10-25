7133c98deebdd4041b7d731247c943a8048c79e5
Wei Zhang <zhang.wei8@northeastern.edu>
2022-10-24 23:25:37 -0400

    Add resize feature.

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,16 +1,17 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
-import { setup } from '../bundler/setup';
 import { bundle } from '../bundler/bundler';
+import Resizeable from './resizable';
+import './resizable.css';
 
 const CodeCell = () => {
- 
+
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
-  
+
   // TODO: Figure out a better way to initialize esbuild only once.
   const onClick = async () => {
     // Bundle the input code and get the result.
@@ -19,20 +20,22 @@ const CodeCell = () => {
   };
 
   return (
-    <div>
-      <CodeEditor
-        initialValue="const a = 1;"
-        onChange={(value) => setInput(value)}
-      />
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
+    <Resizeable direction='vertical'>
       <div>
-        <button onClick={onClick}>Submit</button>
+        <CodeEditor
+          initialValue="const a = 1;"
+          onChange={(value) => setInput(value)}
+        />
+        <textarea
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></textarea>
+        <div>
+          <button onClick={onClick}>Submit</button>
+        </div>
+        <Preview code={code} />
       </div>
-      <Preview code={code}/>
-    </div>
+    </Resizeable>
   );
 };
 
