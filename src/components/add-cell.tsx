9c6e51a83926ceefa82eea0ef79372c3d3636d02
Wei Zhang <zhang.wei8@northeastern.edu>
2022-11-08 13:12:22 -0500

    Refactor the insertbefore code to avoid add button flashing back.

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -2,21 +2,23 @@ import "./add-cell.css"
 import { useActions } from '../hooks/use-actions';
 
 interface AddCellProps {
-    nextCellId: string | null;
+    previousCellId: string | null;
+    // When there is no previous cell, using this to force add cell visible.
+    forceVisible?: boolean;
 }
 
-const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
-    const { insertCellBefore } = useActions();
+const AddCell: React.FC<AddCellProps> = ({ forceVisible, previousCellId }) => {
+    const { insertCellAfter } = useActions();
 
-    return <div className='add-cell'>
+    return <div className={`add-cell ${forceVisible && 'force-visible'}`}>
         <div className="add-buttons">
-            <button className="button is-rounded is-primary is-small" onClick={() => insertCellBefore(nextCellId, 'code')}>
+            <button className="button is-rounded is-primary is-small" onClick={() => insertCellAfter(previousCellId, 'code')}>
                 <span className="icon is-small">
                     <i className="fas fa-plus" />
                 </span>
                 <span>Code</span>
                 </button>
-            <button className="button is-rounded is-primary is-small" onClick={() => insertCellBefore(nextCellId, 'markdown')}>
+            <button className="button is-rounded is-primary is-small" onClick={() => insertCellAfter(previousCellId, 'markdown')}>
                 <span className="icon is-small">
                     <i className="fas fa-plus" />
                 </span>
