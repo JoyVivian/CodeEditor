a6e41b10adba88546cf75471825cb9a08aaad70d
Wei Zhang <zhang.wei8@northeastern.edu>
2022-12-01 22:14:01 -0500

    Ignore some unnecessary folders.

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -23,7 +23,7 @@ const serve = (port, filename, dir, useProxy) => {
     }
     else {
         // Code for production environment.
-        const packagePath = require.resolve("local-client/build/index.html");
+        const packagePath = require.resolve("@codeair/local-client/build/index.html");
         app.use(express_1.default.static(path_1.default.dirname(packagePath)));
         console.log(useProxy);
     }
